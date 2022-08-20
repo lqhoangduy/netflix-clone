165d06d98fa3dad95675d3241d3864cecbf1000c
lqhoangduy <lqhoangduy@gmail.com>
2022-08-20 23:38:25 +0700

    feat: modal trailer

--- components/Row.tsx
+++ components/Row.tsx
@@ -1,6 +1,6 @@
 import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/solid";
 import React, { useRef, useState } from "react";
-import { Movie } from "../typing";
+import { Movie } from "../typings";
 import Thumbnail from "./Thumbnail";
 
 interface Props {
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,12 @@
-import type { NextPage } from "next";
 import Head from "next/head";
-import Image from "next/image";
+import { useRecoilValue } from "recoil";
+import { modalState } from "../atoms/modalAtom";
 import Banner from "../components/Banner";
 import Header from "../components/Header";
+import Modal from "../components/Modal";
 import Row from "../components/Row";
 import useAuth from "../hooks/useAuth";
-import { Movie } from "../typing";
+import { Movie } from "../typings";
 import requests from "../utils/request";
 
 interface Props {
@@ -30,6 +31,7 @@ const Home = ({
 	documentaries,
 }: Props) => {
 	const { loading } = useAuth();
+	const showModal = useRecoilValue(modalState);
 
 	if (loading) return null;
 
@@ -53,6 +55,7 @@ const Home = ({
 					<Row title='Documentaries' movies={documentaries} />
 				</section>
 			</main>
+			{showModal && <Modal />}
 		</div>
 	);
 };
